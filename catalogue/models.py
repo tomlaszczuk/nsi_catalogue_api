a6f7fdf3d8916b359b645b9865dd0d50c637614a
sitrus <tomaszlaszczuk@gmail.com>
2015-07-07 11:20:49 +0200

    Product Model

--- catalogue/models.py
+++ catalogue/models.py
@@ -4,18 +4,18 @@ from django.db import models
 class Promotion(models.Model):
 
     SEGMENT_CHOICES = (
-        ('Abonament Nowy_numer', 'IND.NEW.POSTPAID.ACQ'),
-        ('Abonament Przenieś_numer', 'IND.NEW.POSTPADI.MNP'),
-        ('MIX Nowy_numer', 'IND.NEW.MIX.ACQ'),
-        ('Abonament Przejdź_na_abonament', 'IND.SUB.MIG.POSTPAID'),
-        ('MIX Przejdź_na_MIX', 'IND.SUB.MIG.MIX'),
-        ('Abonament Przedłuż_umowę', 'IND.SUB.RET.POSTPAID'),
-        ('MIX Przedłuż_umowę', 'IND.SUB.RET.MIX'),
-        ('Abonament Dokup_usługę Nowy_numer', 'IND.SUB.SAT.POSTPAID'),
-        ('Dla_firm Abonament Nowy_numer', 'SOHO.NEW.POSTPAID.ACQ'),
-        ('Dla_firm Abonament Przenieś_numer', 'SOHO.NEW.POSTPAID.MNP'),
-        ('Dla_firm Abonament Dokup_usługę Nowy_numer', 'SOHO.SUB.SAT.POSTPAID'),
-        ('Dla_firm Abonamet Przedłuż_umowę', 'SOHO.SUB.RET.POSTPAID')
+        ('IND.NEW.POSTPAID.ACQ', 'Abonament Nowy_numer'),
+        ('IND.NEW.POSTPADI.MNP', 'Abonament Przenieś_numer'),
+        ('IND.NEW.MIX.ACQ', 'MIX Nowy_numer'),
+        ('IND.SUB.MIG.POSTPAID', 'Abonament Przejdź_na_abonament'),
+        ('IND.SUB.MIG.MIX', 'MIX Przejdź_na_MIX'),
+        ('IND.SUB.RET.POSTPAID', 'Abonament Przedłuż_umowę'),
+        ('IND.SUB.RET.MIX', 'MIX Przedłuż_umowę'),
+        ('IND.SUB.SAT.POSTPAID', 'Abonament Dokup_usługę Nowy_numer'),
+        ('SOHO.NEW.POSTPAID.ACQ', 'Dla_firm Abonament Nowy_numer'),
+        ('SOHO.NEW.POSTPAID.MNP', 'Dla_firm Abonament Przenieś_numer'),
+        ('SOHO.SUB.SAT.POSTPAID', 'Dla_firm Abonament Dokup_usługę Nowy_numer'),
+        ('SOHO.SUB.RET.POSTPAID', 'Dla_firm Abonamet Przedłuż_umowę'),
     )
 
     name = models.CharField(max_length=100, blank=True, default='')
@@ -34,6 +34,9 @@ class Promotion(models.Model):
     def save(self, *args, **kwargs):
         self.market = self.process_segmentation.split('.')[0]
 
+    def __str__(self):
+        return self.code
+
 
 class TariffPlan(models.Model):
     promotions = models.ManyToManyField(Promotion, related_name='tariff_plans',
@@ -42,3 +45,30 @@ class TariffPlan(models.Model):
     description = models.CharField(max_length=255, blank=True, default='')
     code = models.CharField(max_length=10, unique=True)
     monthly_fee = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+class Product(models.Model):
+
+    PRODUCT_TYPE_CHOICES = (
+        'PHONE', 'Telefon',
+        'MODEM', 'Modem/Router',
+        'SIM_CARD', 'Starter',
+        'RETAIL', 'Konsola',
+        'BUNDLE', 'Zestaw',
+        'TAB', 'Tablet/Laptop'
+    )
+
+    model_name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=50)
+    product_type = models.CharField(choices=PRODUCT_TYPE_CHOICES, max_length=20,
+                                    default='PHONE')
+    full_name = models.CharField(max_length=150, blank=True, default='')
+
+    class Meta:
+        unique_together = ('model_name', 'manufacturer')
+
+    def save(self, *args, **kwargs):
+        self.full_name = "{0} {1}".format(self.manufacturer, self.model_name)
